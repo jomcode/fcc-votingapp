523255cd123e027aa376d150705d832114d98bab
John <jomcode@gmail.com>
2016-04-29 07:01:07 -0500

    adding client setup. webpack-dev-server, react, babel

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './client/sass/main.scss';
+
+const App = () => (<h1>App</h1>);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const port = process.env.WEBPACK_PORT || 8080;
+
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry: {
+    client: path.join(__dirname, 'src/client.js')
+  },
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    stats: {
+      colors: true,
+      chunks: false
+    },
+    host: 'localhost',
+    port: port
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass', 'postcss'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  postcss: () => [autoprefixer],
+
+  plugins: [
+    new HtmlPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Title',
+      appMountId: 'root',
+      inject: false,
+      mobile: true
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};
