2834844b2bfc12271074567c57e3541a2394fb92
John <jomcode@gmail.com>
2016-04-29 06:20:07 -0500

    express static for public/ folder

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const passport = require('passport');
+const path = require('path');
 
 const registerRouter = require('./server/endpoints/register');
 const authenticateRouter = require('./server/endpoints/authenticate');
@@ -10,6 +11,8 @@ const pollsRouter = require('./server/endpoints/polls');
 require('./server/config/passport')(passport);
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 if (process.env.NODE_ENV === 'production') app.use(morgan('combined'));
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
